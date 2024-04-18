0f00c48db558dfa82ac93fc8d4e820837c7a11b1
Englishman <56271563+RomanEnglishman@users.noreply.github.com>
2024-04-19 00:58:19 +0500

    123

--- lib/utils/thdata.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData basicTheme() => ThemeData(
-      hintColor: Colors.white,
-      buttonTheme: ButtonThemeData(
-        buttonColor: Colors.blueAccent,
-        textTheme: ButtonTextTheme.accent,
-      ),
-    );
